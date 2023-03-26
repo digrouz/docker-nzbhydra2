64c9c24e8e252f9a84884a220236dcfa03b02e83
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2023-03-26 20:30:27 +0200

    avoid empty version

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -6,7 +6,9 @@ NZBHYDRA2_URL="https://api.github.com/repos/theotherp/nzbhydra2/releases"
 FULL_LAST_VERSION=$(curl -SsL ${NZBHYDRA2_URL} | jq .[0].name -r )
 LAST_VERSION="${FULL_LAST_VERSION:1}"
 
-sed -i -e "s|NZBHYDRA2_VERSION='.*'|NZBHYDRA2_VERSION='${LAST_VERSION}'|" Dockerfile*
+if [ "${LAST_VERSION}" ];then
+  sed -i -e "s|NZBHYDRA2_VERSION='.*'|NZBHYDRA2_VERSION='${LAST_VERSION}'|" Dockerfile*
+fi
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
